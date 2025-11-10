df9e7b7feeaaf04e73eab5cc6245e792bce2ffc0
Stopatigh <silpaotop@hotmail.com.ar>
2025-11-10 19:46:14 -0300

    dialog

---
+++ js/dialog.js
@@ -0,0 +1,13 @@
+
+        const formulario = document.querySelector("form");
+        const dialog = document.querySelector("dialog");
+        const cerrar = document.querySelector("#js-CloseDialog");
+
+        formulario.addEventListener("submit", (event) => {
+        event.preventDefault();
+        dialog.showModal();
+        });
+
+        cerrar.addEventListener("click", (event) => {
+        dialog.close();
+        });
