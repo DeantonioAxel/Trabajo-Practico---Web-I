ca897657d2345b0a0dd1bdd7318dc96243bf0f47
Stopatigh <silpaotop@hotmail.com.ar>
2025-11-11 23:39:24 -0300

    inscripcion empresas

---
+++ js/inscripcionEmpresas.js
@@ -0,0 +1,17 @@
+
+
+const cursosDisponibles = [
+    {id: 1, nombre: "Canva", precio: 29.99},
+    {id: 2, nombre: "Blenderdiseño3d", precio: 69.99},
+    {id: 3, nombre: "JavaScript", precio: 59.99},
+    {id: 4, nombre: "Python", precio: 59.99},
+    {id: 5, nombre: "Chat Gpt", precio: 49.99},
+    {id: 6, nombre: "Illustrator", precio: 49.99},
+    {id: 7, nombre: "Photoshop", precio: 39.99},
+    {id: 8, nombre: "Java", precio: 59.99},
+    {id: 9, nombre: "Programacion C", precio: 49.99},
+    {id: 10, nombre: "Inteligencia artificial (AI)", precio: 69.99},
+    {id: 11, nombre: "AI: Generación de Prompts", precio: 39.99},
+    {id: 12, nombre: "AI: Marketing Digital", precio: 59.99},
+]
+
