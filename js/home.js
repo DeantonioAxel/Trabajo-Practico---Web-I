685298a129584cb4bd59e25ba3502662df040673
fernando Aybar <fernandoaybar96@gmail.com>
2025-11-11 22:24:57 -0300

    carruselito

--- js/home.js
+++ js/home.js
@@ -1,5 +1,10 @@
 import { IniciarSesion } from "./iniciarSesion.js";
+import { Slider } from "./slider.js";
 
 const iniciarSesion = new IniciarSesion();
+
+// document.addEventListener('DOMContentLoaded', ()=>{
+const slider =new Slider();
+// });
 
 iniciarSesion.render();
