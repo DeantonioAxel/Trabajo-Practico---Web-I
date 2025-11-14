d9f27709edc4ec851bec9f99816f19fc78ee2cbe
DeantonioAxel <axel.gdeantonio@gmail.com>
2025-11-14 20:37:58 -0300

    slider arreglado

--- js/home.js
+++ js/home.js
@@ -1,11 +1,8 @@
 
-import { IniciarSesion } from "./iniciarSesion.js";
 import { Slider } from "./slider.js";
 
-const iniciarSesion = new IniciarSesion();
+document.addEventListener('DOMContentLoaded', () => {
+    const slider = new Slider();
 
-// document.addEventListener('DOMContentLoaded', ()=>{
-const slider =new Slider();
-// });
-
-iniciarSesion.render();
+    slider.render();
+});
