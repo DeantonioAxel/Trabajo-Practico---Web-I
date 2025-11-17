b4834d0723fd821e81d9b9412e8c70b2eea5275c
DeantonioAxel <axel.gdeantonio@gmail.com>
2025-11-16 22:31:34 -0300

    falta terminar

--- js/dialog.js
+++ js/dialog.js
@@ -1,6 +1,6 @@
 export class Dialog {
         constructor() {
-                const formulario = document.querySelector("form");
+                const formulario = document.querySelector(".form");
                 const dialog = document.querySelector("dialog");
                 const cerrar = document.querySelector("#js-CloseDialog");
 
@@ -11,7 +11,7 @@ export class Dialog {
 
                 cerrar.addEventListener("click", (event) => {
                         dialog.close();
-                        form.reset();
+                        formulario.reset();
                 });
         }
 }
--- js/formulario.js
+++ js/formulario.js
@@ -4,16 +4,24 @@ import { generarFormulario } from './generarFormulario.js';
 import { HeaderOtrasVistas } from "./headerOtrasVistas.js";
 
 const header = new HeaderOtrasVistas();
-
 const cursoSeleccionado = JSON.parse(localStorage.getItem("cursoSeleccionado"));
+const btnInscripcion = document.querySelector('.inscription');
 
 if(cursoSeleccionado){
     generarFormulario(cursoSeleccionado);
+
+    const mensaje = new Dialog();
+
+    btnInscripcion.addEventListener('click', () => {
+        const usuarioLogueado = JSON.parse(localStorage.getItem('usuarioLogueado'));
+
+        usuarioLogueado.cursos.push(cursoSeleccionado);
+
+        localStorage.setItem('usuarioLogueado', JSON.stringify(usuarioLogueado));
+    })
 } else {
     console.warn("No hay curso seleccionado");
 }
-
-const mensaje = new Dialog();
 
 header.render();
 
