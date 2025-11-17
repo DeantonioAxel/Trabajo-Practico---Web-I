2f536149f916f0496656f956aa12fd534b64f586
solbenitez04 <solbenitez2004@gmail.com>
2025-11-17 00:43:26 -0300

    medios de pago

--- js/home.js
+++ js/home.js
@@ -2,12 +2,15 @@ import { HeaderHome } from "./headerHome.js";
 import { cursosDisponibles } from "./cursos.js";
 import { Slider } from "./slider.js";
 import { mostrarPrimerosNCursos } from "./mostrarCursos.js";
+import { MediosPagosSlider } from "./mediosPagosSlider.js";
 
 const header = new HeaderHome();
 const slider = new Slider();
+const mediosPagosSlider = new MediosPagosSlider();
 
 slider.render();
 header.render();
+mediosPagosSlider.init();
 
 
 document.addEventListener('DOMContentLoaded', () => {
